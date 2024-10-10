8c509bb8e8bac40f6ebd33ef410707e0cbd105bc
Reversive <matiaspavan@gmail.com>
2024-10-10 19:45:34 -0300

    feat: added extra measurements

--- src/main/java/ar/edu/itba/Main.java
+++ src/main/java/ar/edu/itba/Main.java
@@ -1,12 +1,20 @@
 package ar.edu.itba;
 
 import ar.edu.itba.implementations.ForkMeasurement;
+import ar.edu.itba.implementations.ParallelMeasurement;
+import ar.edu.itba.implementations.SequentialMeasurement;
 
 public class Main {
     public static void main(String[] args) {
         int[] threads = {2, 4, 8, 16};
         int[] thresholds = {64};
-        ForkMeasurement measurement = new ForkMeasurement(100, threads, thresholds);
-        measurement.measure();
+        ForkMeasurement forkMeasurement = new ForkMeasurement(100, threads, thresholds);
+        forkMeasurement.measure();
+
+        ParallelMeasurement parallelMeasurement = new ParallelMeasurement(100, threads);
+        parallelMeasurement.measure();
+
+        SequentialMeasurement sequentialMeasurement = new SequentialMeasurement(100);
+        sequentialMeasurement.measure();
     }
 }
